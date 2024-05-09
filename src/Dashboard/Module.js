edd564e8521518db6316d9cb0073c95203111cfa
kaivan <kaivanshah1663@gmail.com>
2024-05-09 22:48:59 +0530

    Removing unsued dependency

--- src/Dashboard/Module.js
+++ src/Dashboard/Module.js
@@ -1,31 +1,43 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import {
   Typography,
   Card,
   CardContent,
   CardActions,
   Button,
+  styled,
 } from "@mui/material";
-import { makeStyles } from "@mui/styles";
 import CryptoJS from "crypto-js";
 
-
-const useStyles = makeStyles({
-  cardStyle: {
-    "&:hover": {
-      boxShadow: "0 8px 16px 0 rgba(0, 0, 0, 0.2)",
-      transform: "translateY(-4px)",
-    },
-  },
-  launchButton: {
-    "&:hover": {
-      backgroundColor: "grey",
-      color: "white",
-    },
+const StyledCard = styled(Card)({
+  width: "300px",
+  margin: "5% 10%",
+  display: "flex",
+  flexDirection: "column",
+  height: "100%",
+  borderRadius: "7%",
+  border: "1px solid black",
+  transition: "0.3s",
+  boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+  "&:hover": {
+    boxShadow: "0 8px 16px 0 rgba(0, 0, 0, 0.2)",
+    transform: "translateY(-4px)",
   },
 });
 
-const encryptData = (data, secretKey,setError) => {
+const LaunchButton = styled(Button)({
+  backgroundColor: "black",
+  color: "white",
+  borderRadius: "0.5rem",
+  cursor: "pointer",
+  padding: "0.5rem 2rem",
+  "&:hover": {
+    backgroundColor: "grey",
+    color: "white",
+  },
+});
+
+const encryptData = (data, secretKey, setError) => {
   try {
     const jsonData = JSON.stringify(data);
     const encryptedData = CryptoJS.AES.encrypt(jsonData, secretKey).toString();
@@ -35,8 +47,7 @@ const encryptData = (data, secretKey,setError) => {
   }
 };
 
-const Module = ({ name, description, userEmail, username,module }) => {
-  const classes = useStyles();
+const Module = ({ name, description, userEmail, username, module }) => {
   const [error, setError] = useState(false);
   const handleLaunch = async (e) => {
     e.preventDefault();
@@ -46,7 +57,8 @@ const Module = ({ name, description, userEmail, username,module }) => {
         username: username,
       };
       const secretKey = process.env.REACT_APP_SECRET_KEY;
-      const encryptedData = encryptData(sessionInfo, secretKey,setError);
+      const encryptedData = encryptData(sessionInfo, secretKey, setError);
+      console.log(encryptedData)
       window.location.href = `${module.link}?data=${encryptedData}`;
     } catch (error) {
       setError(true);
@@ -54,80 +66,42 @@ const Module = ({ name, description, userEmail, username,module }) => {
   };
 
   return (
-    <Card
-      style={{
-        width: "85%",
-        margin: "5% 10%",
-        display: "flex",
-        flexDirection: "column",
-        height: "100%",
-        borderRadius: "7%",
-        border: "1px solid black",
-        transition: "0.3s",
-        boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
-      }}
-      className={classes.cardStyle}
-    >
+    <StyledCard>
       <CardContent style={{ flexGrow: 1 }}>
         <Typography
           gutterBottom
           variant="h5"
           component="h2"
-          style={{
-            fontWeight: "bold",
-            textAlign: "center",
-          }}
+          style={{ fontWeight: "bold", textAlign: "center" }}
         >
           {name}
         </Typography>
       </CardContent>
       {error ? (
-        <>
+        <Typography
+          variant="body2"
+          color="error"
+          style={{ textAlign: "justify", margin: "0% 2% 40%" }}
+        >
+          Error launching module. Please try again later.
+        </Typography>
+      ) : (
+        <CardContent>
           <Typography
             variant="body2"
-            color="error"
-            style={{
-              textAlign: "justify",
-              margin: "0% 2% 40%",
-            }}
+            component="p"
+            style={{ textAlign: "justify", margin: "0% 2% 40%" }}
           >
-            Error launching module. Please try again later.
+            {description}
           </Typography>
-        </>
-      ) : (
-        <>
-          <CardContent>
-            <Typography
-              variant="body2"
-              component="p"
-              style={{
-                textAlign: "justify",
-                margin: "0% 2% 40%",
-              }}
-            >
-              {description}
-            </Typography>
-          </CardContent>
-        </>
+        </CardContent>
       )}
-
       <CardActions style={{ justifyContent: "center", paddingBottom: "2%" }}>
-        <Button
-          size="small"
-          style={{
-            backgroundColor: "black",
-            color: "white",
-            borderRadius: "0.5rem",
-            cursor: "pointer",
-            padding: "0.5rem 2rem",
-          }}
-          onClick={handleLaunch}
-          className={classes.launchButton}
-        >
+        <LaunchButton size="small" onClick={handleLaunch}>
           Launch
-        </Button>
+        </LaunchButton>
       </CardActions>
-    </Card>
+    </StyledCard>
   );
 };
 
